4252e0df36fce62e7aca93c18a896b121efa6e3f
Smith <AnsonSmith@quickenloans.com>
2018-12-04 16:45:51 -0500

    re-added day 1 solutions

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -1,18 +1,26 @@
 defmodule Aoc2018.Day1 do
-  @moduledoc """
-  Documentation for Aoc2018.
-  """
+  def part_1() do
+      get_input()
+      |> Stream.map(&String.to_integer/1)
+      |> Enum.sum()
+  end
 
-  @doc """
-  Hello world.
+  def part_2() do
+    get_input()
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.cycle()
+    |> Enum.reduce_while({MapSet.new(), 0}, fn(x, {previous_freqs, curr_freq}) ->
+      new_freq = curr_freq + x
+      if MapSet.member?(previous_freqs, new_freq) do
+        {:halt, new_freq}
+      else
+        {:cont, {MapSet.put(previous_freqs, new_freq), new_freq}}
+      end
+    end)
+  end
 
-  ## Examples
-
-      iex> Aoc2018.hello
-      :world
-
-  """
-  def part_1 do
-    "Accidentally deleted because I'm an idiot"
+  defp get_input() do
+    File.stream!("priv/day_1_input.dat")
+    |> Stream.map(&String.trim/1)
   end
 end
